d32527a1baad5be499b92cba56afa3219adf92fd
Pulipuli Chen <blog@pulipuli.info>
2019-09-13 21:04:08 +0800

    0913 close #32

--- app/helpers/autoit/WindowsShortcutHelper.js
+++ app/helpers/autoit/WindowsShortcutHelper.js
@@ -5,7 +5,8 @@ let WindowsShortcutHelper = {
     path: null,
     exec: null,
     iconv: null,
-    WindowsEnvVarHelper: null
+    WindowsEnvVarHelper: null,
+    imageSize: null,
   },
   shortcutReaderPath: null,
   init: function () {
@@ -17,6 +18,7 @@ let WindowsShortcutHelper = {
     this.lib.path = require('path')
     this.lib.exec = require('child_process').exec
     this.lib.iconv = require('iconv-lite')
+    this.lib.imageSize = require('image-size')
     
     this.lib.WindowsEnvVarHelper = RequireHelper.require('./helpers/autoit/WindowsEnvVarHelper')
     
@@ -63,12 +65,60 @@ let WindowsShortcutHelper = {
         result[key] = value
       })
       
+      
       result['Path'] = lnkPath
+      
+      let altPath = this.getAlternativeIcon(result['Icon'])
+      if (typeof(altPath) === 'string') {
+        result['Icon'] = altPath
+      }
+      else if (result['Target'].endsWith('chrome.exe') 
+              && result['Target'] !== result['Exec']) {
+        // do nothing
+      }
+      else {
+        altPath = this.getAlternativeIcon(result['Target'])
+        if (typeof(altPath) === 'string') {
+          result['Icon'] = altPath
+        }
+      }
       
       if (typeof(callback) === 'function') {
         callback(result)
       }
     })
+    return this
+  },
+  getAlternativeIcon: function (anchorPath) {
+    if (typeof(anchorPath) !== 'string' 
+            || this.lib.fs.existsSync(anchorPath) === false
+            || anchorPath.lastIndexOf('.') === -1) {
+      return false
+    }
+    
+    let pathWithoutExt = anchorPath.slice(0, anchorPath.lastIndexOf('.') + 1)
+    let alternativeList = [
+      pathWithoutExt + 'png',
+      pathWithoutExt + 'jpg',
+      pathWithoutExt + 'jpeg',
+      pathWithoutExt + 'gif',
+    ]
+    
+    let width = 0
+    if (anchorPath.endsWith('.ico')) {
+      width = this.lib.imageSize(anchorPath).width
+    }
+    else if (anchorPath.endsWith('chrome.exe') === false) {
+      alternativeList.push(pathWithoutExt + 'ico')
+    }
+    
+    for (let i = 0; i < alternativeList.length; i++) {
+      let altPath = alternativeList[i]
+      if (this.lib.fs.existsSync(altPath) 
+              && this.lib.imageSize(altPath).width > width) {
+        return altPath
+      }
+    }
   }
 }
 
